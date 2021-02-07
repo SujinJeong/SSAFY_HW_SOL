db4572c5646c425f4925a277af99d29d3ea1623e
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2021-02-08 00:40:08 +0900

    2/8 순열, 조합, 부분집합 코드 수정 -- 수진

--- HWProject/src/com/ssafy/SOL_0205/Per_Comb_Power.java
+++ HWProject/src/com/ssafy/SOL_0205/Per_Comb_Power.java
@@ -3,16 +3,17 @@ package com.ssafy.SOL_0205;
 import java.util.Arrays;
 
 public class Per_Comb_Power {
-	static int[] num = { 1, 2, 3 }; // num = {1,2,3,4}
-	static int N = 3; // N=2, N=3
+	static int[] num = { 1, 2, 3}; // num = {1,2,3,4}
+	static int N = 2; // N=2, N=3
 	static int sCount = 0;
 
+	// cnt: 현재까지 처리한 원소개수
 	static void powerSet(int cnt, boolean[] visited) {
-	    if(cnt == N) {
+	    if(cnt == num.length) {
 	    	sCount++;
 	    	
 	    	System.out.print("{");
-	    	for (int i = 0; i < N; i++) {
+	    	for (int i = 0; i < num.length; i++) {
 	    		if (visited[i])
 	    			System.out.print(num[i] + ",");
 	    	}
@@ -30,31 +31,31 @@ public class Per_Comb_Power {
 	    powerSet(cnt+1, visited);
 	}
 	
-	public static void makePermutation(int start, int[] arr, boolean[] visited) {
-		if (start == N) {
-			System.out.println(Arrays.toString(arr));
+	public static void makePermutation(int cnt, int[] selected, boolean[] visited) {
+		if (cnt == N) {
+			System.out.println(Arrays.toString(selected));
 			return;
 		}
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < num.length; i++) {
 			if (!visited[i]) {
 				visited[i] = true;
-				arr[start] = num[i];
-				makePermutation(start + 1, arr, visited);
+				selected[cnt] = num[i];
+				makePermutation(cnt + 1, selected, visited);
 				visited[i] = false;
 			}
 		}
 	}
 
 	// cnt가 nCr에서 r의 의미, arr는 visited와 비슷 애초에 뽑힌것만 arr에 담아줌
-	public static void makeCombination(int cnt, int[] arr, int start) {
+	public static void makeCombination(int cnt, int[] selected, int start) {
 		if (cnt == N) {
-			System.out.println(Arrays.toString(arr));
+			System.out.println(Arrays.toString(selected));
 			return;
 		}
 
-		for (int i = start; i < arr.length; i++) {
-			arr[start] = num[i];
-			makeCombination(cnt + 1, arr, start + 1);
+		for (int i = start; i < num.length; i++) {
+			selected[cnt] = num[i];
+			makeCombination(cnt + 1, selected, i + 1);
 		}
 	}
 
@@ -69,7 +70,7 @@ public class Per_Comb_Power {
 		
 //      3. num으로 구성할 수 있는 모든 부분집합을 출력하시오.
 		System.out.println("-----부분집합-----");
-		powerSet(0, new boolean[N]);
+		powerSet(0, new boolean[num.length]);
 
 	}
 
