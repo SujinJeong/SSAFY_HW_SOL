56c1e15de915de6998e1c9e10354829f28689cbb
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2021-02-26 03:36:02 +0900

    2/26 Q9655 -- 수진

---
+++ HWProject/src/com/ssafy/SOL_0225/Q9655.java
@@ -0,0 +1,15 @@
+package com.ssafy.SOL_0225;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9655 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+	}
+
+}
